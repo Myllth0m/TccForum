22e1ff5f0d4678d1eaf890b06d963214521c2a2c
Milton D. Yama <Miltondyama@outlook.com>
2024-10-04 11:23:49 -0400

    criei e apliquei a migration inital do projeto

---
+++ TccForum/Migrations/20241004152249_CriacaoDoBanco.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TccForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoDoBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Perguntas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Capa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Perguntas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Respostas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Conteudo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PerguntaId = table.Column<int>(type: "int", nullable: false),
+                    RespostaPaiId = table.Column<int>(type: "int", nullable: true),
+                    RespostaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Respostas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Respostas_Perguntas_PerguntaId",
+                        column: x => x.PerguntaId,
+                        principalTable: "Perguntas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Respostas_Respostas_RespostaId",
+                        column: x => x.RespostaId,
+                        principalTable: "Respostas",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Respostas_PerguntaId",
+                table: "Respostas",
+                column: "PerguntaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Respostas_RespostaId",
+                table: "Respostas",
+                column: "RespostaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Respostas");
+
+            migrationBuilder.DropTable(
+                name: "Perguntas");
+        }
+    }
+}
