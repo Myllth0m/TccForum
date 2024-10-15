42c3509430672d7b7295379458da38c6ddec0a76
1000THOM <52299116+Myllth0m@users.noreply.github.com>
2024-10-15 02:31:57 -0400

    fiz a partial view de cadastro de uma resposta da pergunta

---
+++ TccForum/Controllers/RespostaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TccForum.Models.ViewModels;
+
+namespace TccForum.Controllers
+{
+    public class RespostaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastrar(RespostaDaPerguntaCriacaoViewModel respostaDaPerguntaCriacaoViewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ TccForum/Models/ViewModels/RespostaDaPerguntaCriacaoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TccForum.Models.ViewModels
+{
+    public class RespostaDaPerguntaCriacaoViewModel
+    {
+        public string Conteudo { get; set; }
+        public int PerguntaId { get; set; }
+    }
+}
