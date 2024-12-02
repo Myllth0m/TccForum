6ae0d03748a40d665817fae21bfb7720b74b8742
milton <mlitondyama@gmail.com>
2024-12-02 11:44:10 -0400

    finalizei a estilizacao da tela de login

--- TccForum/Models/ViewModels/AcessoViewModel.cs
+++ TccForum/Models/ViewModels/AcessoViewModel.cs
@@ -1,8 +1,14 @@
-﻿namespace TccForum.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TccForum.Models.ViewModels
 {
     public class AcessoViewModel
     {
+        [Required(ErrorMessage = "Nome de usuário/e-mail obrigatório!!")]
+        [Display(Name = "Nome de usuário ou e-mail")]
         public string NomeDoUsuarioOuEmail { get; set; }
+
+        [Required(ErrorMessage = "Senha obrigatória!!")]
         public string Senha { get; set; }
     }
 }
